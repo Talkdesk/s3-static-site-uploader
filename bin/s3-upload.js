606dd4406a5682105adea715e8bf839c76794b08
trenpixster <nizar.venturini@gmail.com>
2015-08-07 10:45:35 +0100

    Allow to specify target bucket

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
 var ConfigRunner = require('../src/ConfigRunner.js');
 var path = require('path');
-
+var custom_bucket = null;
 var runner = new ConfigRunner();
 
+if (process.argv.length > 2){
+  custom_bucket = process.argv[2];
+}
 
 var configPath = path.resolve('./aws-upload.conf.js');
-
 var config = require(configPath);
+
+var bucketName = custom_bucket || config.bucketName;
+config.bucketName = bucketName
+
+console.log("Publishing to ", bucketName);
 
 runner.setConfig(config);
 
